e91248b1533e8ec2fd4cc057322d1c42f1914066
Alan Richardson <alanr@ferstaberinde.com>
2017-11-12 12:46:31 +0000

    Added welcome message

--- Microsoft.PowerShell_Profile.ps1
+++ Microsoft.PowerShell_Profile.ps1
@@ -14,3 +14,5 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+Write-Out "Powershell profile loaded!"
