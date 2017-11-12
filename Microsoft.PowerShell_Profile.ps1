5b72d1ba52e80ed7e5f0d39a5723e5bcbf673eeb
Alan Richardson <alan.richardson@rnicg.com>
2017-11-12 12:21:15 +0000

    Initial Commit

---
+++ Microsoft.PowerShell_Profile.ps1
@@ -0,0 +1,16 @@
+#requires -modules oh-my-posh
+Set-Theme agnoster
+
+#PSReadline Stuff
+Set-PSReadlineOption -TokenKind Command -ForeGroundColor Cyan
+Set-PSReadlineOption -EditMode EMACS
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key CTRL+V -Function Paste
+Set-PSReadlineKeyHandler -Key CTRL+RightArrow -Function ForwardWord
+Set-PSReadlineKeyHandler -Key CTRL+LeftArrow -Function BackwardWord
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
