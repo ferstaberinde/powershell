b32ef97ac9dbbf90a4e83ed9631dc955655cf172
ferstaberinde <alanr@ferstaberinde.com>
2017-11-14 00:38:56 +0000

    Updated Get-O365Groups

--- Get-O365GroupsToCSV.psm1
+++ Get-O365GroupsToCSV.psm1
@@ -23,10 +23,19 @@ Function Get-O365GroupsToCSV {
     $CsvName = $Filename + ".csv"
 # This command returns all the Unified Groups (O365 Groups)
     $UnifiedGroups = Get-UnifiedGroup
-# This block builds an array of custom objects, each of which has properties for 'DisplayName' and 'AccessType' 
+# This block builds an array of custom objects, each of which has members for all the Group properties available via the Get-UnifiedGroup cmdlet 
     $GroupOutput = @()
     $UnifiedGroups | ForEach-Object {
-        $ThisGroup = New-Object -Type PSCustomObject -Property @{'DisplayName' = $_.DisplayName ; 'AccessType' = $_.AccessType}
+        $ThisGroup = New-Object -Type PSCustomObject
+        $ThisGroup | Add-Member -NotePropertyName DisplayName -NotePropertyValue $_.DisplayName
+        $ThisGroup | Add-Member -NotePropertyName AccessType -NotePropertyValue $_.AccessType
+        $ThisGroup | Add-Member -NotePropertyName Description -NotePropertyValue $_.Notes
+        $ThisGroup | Add-Member -NotePropertyName Owners -NotePropertyValue $_.ManagedBy
+        $ThisGroup | Add-Member -NotePropertyName MembersCount -NotePropertyValue $_.GroupMemberCount 
+        $ThisGroup | Add-Member -NotePropertyName ExtMembersCount -NotePropertyValue $_.GroupExternalMemberCount
+        # $ThisGroup | Add-Member -NotePropertyName Email -NotePropertyValue $_.EmailAddresses
+        $ThisGroup | Add-Member -NotePropertyName PrimaryEmail -NotePropertyValue $_.PrimarySmtpAddress
+        $ThisGroup | Add-Member -NotePropertyName AutoSub -NotePropertyValue $_.AutoSubscribeNewMembers
         $GroupOutput += $ThisGroup
     }
 # This block attempts to output the array of custom objects into a CSV file, then closes the PSSession IF it was created by this script
